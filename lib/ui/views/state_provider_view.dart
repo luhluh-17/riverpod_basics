cbfca0d9c4fa3b4d99381066ebb127f7561d46ee
luhluh-17 <castro.ralph17@gmail.com>
2023-06-27 19:57:53 +0800

    add state provider sample

--- lib/ui/views/state_provider_view.dart
+++ lib/ui/views/state_provider_view.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class StateProviderView extends StatelessWidget {
+// user autoDispose to dispose of the state when no longer needed
+final valueStateProvider = StateProvider.autoDispose<int>((ref) => 0);
+
+// final valueStateProvider = StateProvider<int>((ref) => 42);
+
+class StateProviderView extends ConsumerWidget {
   const StateProviderView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(valueStateProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('State Provider'),
       ),
-      body: const Placeholder(),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              value.toString(),
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(valueStateProvider.notifier).state++;
+              },
+              child: const Text('Increment'),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+              ),
+              onPressed: () {
+                ref.invalidate(valueStateProvider);
+              },
+              child: const Text('Invalidate'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
