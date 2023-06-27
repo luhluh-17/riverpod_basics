15c6cc903f9b9fb04da5e6b254a6c8530f3d7c01
luhluh-17 <castro.ralph17@gmail.com>
2023-06-27 19:25:02 +0800

    completed provider sample

--- lib/ui/views/provider_view.dart
+++ lib/ui/views/provider_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+final valueProvider = Provider<int>((ref) => 42);
+
+// Basic Provider is for Readonly data
 class ProviderView extends StatelessWidget {
   const ProviderView({super.key});
 
@@ -9,7 +13,39 @@ class ProviderView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Provider'),
       ),
-      body: const Placeholder(),
+      body: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            final value = ref.watch(valueProvider);
+            return Text(
+              value.toString(),
+              style: Theme.of(context).textTheme.headlineMedium,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// Second Example using ConsumerWidget
+class ProviderView2 extends ConsumerWidget {
+  const ProviderView2({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(valueProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider'),
+      ),
+      body: Center(
+        child: Text(
+          value.toString(),
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
     );
   }
 }
